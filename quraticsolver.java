c539ca918d6a7b0d29c13031648c8c2ddc6b4a92
Taylor Williams <taylorwilliams612@gmail.com>
2022-10-20 17:02:26 -0400

    "b=15"

--- quraticsolver.java
+++ quraticsolver.java
@@ -4,7 +4,7 @@ public class quraticsolver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 7;
-		int b = 9;
+		int b = 15;
 		int c = 3;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
